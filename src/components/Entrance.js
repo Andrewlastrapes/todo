d103c3b5900c6d761eccf03731c1447378345b7c
Andrew Lastrapes <Andrewlastrapes@gmail.com>
2019-01-31 16:16:26 -0500

    Added login component

--- src/components/Entrance.js
+++ src/components/Entrance.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 import { Card, CardBody, Button, CardTitle, CardText, CardImg, Container, CardImgOverlay, Jumbotron } from 'reactstrap';
-import Register from "./Register"
+import Register from "./Register";
+import Login from "./Login";
 import "../css/Entrance.css"
 
 
 class Entrance extends Component {
 
+    state = {
+        register: true
+    }
+
+    constructor(props){
+        super(props);
+        this.toggleRegister = this.toggleRegister.bind(this);
+    }
+
+    toggleRegister(){
+        this.setState({
+            register: !this.state.register
+        })
+    }
+
     render() {
+        const { register } = this.state
         return (
             <div>
                 <Container>
@@ -17,7 +34,7 @@ class Entrance extends Component {
                             <h1 className="display-3">ToDo!</h1>
                             <p className="lead">Keep track of your daily tasks</p>
                         </div>
-                        <div><Register></Register></div>
+                        {register ? <div><Register toggleRegister={this.toggleRegister}></Register></div> : <div><Login></Login></div>}
                         </CardImgOverlay>
                     </Card>
                 </Container>
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,14 +2,13 @@ import React, { Component } from "react";
 
 import { Card, CardBody, CardSubtitle, CardTitle, CardText, CardImg, Container, CardImgOverlay, Col, Button, Form, FormGroup, Label, Input, FormText, Badge } from 'reactstrap';
 
-class Register extends Component {
+class Login extends Component {
 
     render() {
-            const { toggleRegister } = this.props
         return (
             <Container>
 
-                <div className="text-muted display-3">Register</div>
+                <div className="text-muted display-3">Login</div>
                         <Form>
                             <FormGroup row>
                                 <Label for="Email" className="text-muted" sm={2}>Email</Label>
@@ -23,16 +22,7 @@ class Register extends Component {
                                     <Input type="password" name="password" id="Password" />
                                 </Col>
                             </FormGroup>
-                            <FormGroup row>
-                                <Label for="Confirm-Password" className="text-muted" sm={2}>Confirm Password</Label>
-                                <Col sm={4}>
-                                    <Input type="confirm-password" name="confirm-password" id="confirm-Password" />
-                                </Col>
-                            </FormGroup>
-                            <Button size="lg">Submit</Button>
-                            <div className="alreadyRegistered">
-                                <h5 className="text-muted">Already registered? <Badge color="secondary" onClick={toggleRegister}>Sign in</Badge></h5>
-                            </div>
+                            <Button size="lg">Login</Button>
                         </Form>
                    
             </Container>
@@ -41,4 +31,4 @@ class Register extends Component {
 
 }
 
-export default Register;
+export default Login;
